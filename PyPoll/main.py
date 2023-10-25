6b345336c9efe6379ea5b11ad616ae341006432b
cisnerosjp <cisneros.jason.p@gmail.com>
2023-10-25 15:59:23 -0500

    pushing some changes to pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,3 +4,6 @@ from pathlib import Path
 import csv
 
 election_data = Path("Resources/election_data.csv")
+
+with open(election_data, encoding="UTF-8") as csvfile:
+    elections = csv.reader(csvfile, delimiter=",")
